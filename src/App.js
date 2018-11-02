f298f440f67e6e2bbdfa2e9dc037896a4feefd61
Michael <chiangshtaa@gmail.com>
2018-11-02 00:18:31 -0700

    implement smooth scroll across all browsers

--- src/App.js
+++ src/App.js
@@ -7,13 +7,44 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
-// import { Grid, Row, Col } from 'react-bootstrap';
 
 // import './App.css';
 import './App2.css';
 
 
+
 class App extends Component {
+
+
+  smoothScrollTo(target, duration) {
+    // let targetElement = document.querySelector('#' + target);
+    let targetElement = document.getElementById(target);
+    // let targetPosition = targetElement.getBoundingClientRect().top;
+    let targetPosition = targetElement.offsetTop;
+    let startPosition = window.pageYOffset;
+    let distance = targetPosition - startPosition;
+    let startTime = null;
+
+    console.log('targetPos', targetPosition);
+
+    function animation(currentTime) {
+      if (startTime === null) startTime = currentTime;
+      let timeElapsed = currentTime - startTime;
+      let run = ease(timeElapsed, startPosition, distance, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return c / 2 * t * t + b;
+      t--;
+      return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+  }
+
   render() {
     return (
       <div>
@@ -21,11 +52,11 @@ class App extends Component {
           <div className="table-cell">
           <h3> Michael Chiang </h3>
             <div className="nav">
-              <li><a href="#home"> Home </a></li>
-              <li><a href="#about"> About </a></li>
-              <li><a href="#skills"> Skills </a></li>
-              <li><a href="#projects"> Projects </a></li>
-              <li><a href="#contact"> Contact </a></li>
+              <li onClick={() => this.smoothScrollTo('home', 1000)}> Home </li>
+              <li onClick={() => this.smoothScrollTo('about', 1000)}> About </li>
+              <li onClick={() => this.smoothScrollTo('skills', 1000)}> Skills </li>
+              <li onClick={() => this.smoothScrollTo('projects', 1000)}> Projects </li>
+              <li onClick={() => this.smoothScrollTo('contact', 1000)}> Contact </li>
             </div>
           </div>
         </div>   
