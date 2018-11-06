49ed47b93ce5369127ce7f6fc93a9b699bf0dd8d
Michael <chiangshtaa@gmail.com>
2018-11-05 16:09:15 -0800

    modularize navbar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import Navbar from './components/CustomNavbar';
+import NavBar from './components/NavBar';
 import Home from './components/Home';
 import About from './components/About';
 import Skills from './components/Skills';
@@ -14,51 +14,11 @@ import './App2.css';
 
 
 class App extends Component {
-
-
-  smoothScrollTo(target, duration) {
-    // let targetElement = document.querySelector('#' + target);
-    let targetElement = document.getElementById(target);
-    // let targetPosition = targetElement.getBoundingClientRect().top;
-    let targetPosition = targetElement.offsetTop;
-    let startPosition = window.pageYOffset;
-    let distance = targetPosition - startPosition;
-    let startTime = null;
-
-    console.log('targetPos', targetPosition);
-
-    function animation(currentTime) {
-      if (startTime === null) startTime = currentTime;
-      let timeElapsed = currentTime - startTime;
-      let run = ease(timeElapsed, startPosition, distance, duration);
-      window.scrollTo(0, run);
-      if (timeElapsed < duration) requestAnimationFrame(animation);
-    }
-
-    function ease(t, b, c, d) {
-      t /= d / 2;
-      if (t < 1) return c / 2 * t * t + b;
-      t--;
-      return -c / 2 * (t * (t - 2) - 1) + b;
-    }
-
-    requestAnimationFrame(animation);
-  }
-
   render() {
     return (
       <div>
         <div id="left">
-          <div className="table-cell">
-          <h3> Michael Chiang </h3>
-            <div className="nav">
-              <li onClick={() => this.smoothScrollTo('home', 1000)}> Home </li>
-              <li onClick={() => this.smoothScrollTo('about', 1000)}> About </li>
-              <li onClick={() => this.smoothScrollTo('skills', 1000)}> Skills </li>
-              <li onClick={() => this.smoothScrollTo('projects', 1000)}> Projects </li>
-              <li onClick={() => this.smoothScrollTo('contact', 1000)}> Contact </li>
-            </div>
-          </div>
+          <NavBar />
         </div>   
 
         <div id="right">
@@ -68,7 +28,7 @@ class App extends Component {
           <Projects />
           <Contact />
         </div>
-    </div>
+      </div>
     );
   }
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+
+import './css/NavBar.css';
+
+class Navbar extends Component {
+
+  smoothScrollTo(target, duration) {
+    // let targetElement = document.querySelector('#' + target);
+    let targetElement = document.getElementById(target);
+    // let targetPosition = targetElement.getBoundingClientRect().top;
+    let targetPosition = targetElement.offsetTop;
+    let startPosition = window.pageYOffset;
+    let distance = targetPosition - startPosition;
+    let startTime = null;
+
+    console.log('targetPos', targetPosition);
+
+    function animation(currentTime) {
+      if (startTime === null) startTime = currentTime;
+      let timeElapsed = currentTime - startTime;
+      let run = ease(timeElapsed, startPosition, distance, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return c / 2 * t * t + b;
+      t--;
+      return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+  }
+
+  render() {
+    return (
+      <div className="table-cell">
+        <h3> Michael Chiang </h3>
+        <div className="nav">
+          <li onClick={() => this.smoothScrollTo('home', 1000)}> Home </li>
+          <li onClick={() => this.smoothScrollTo('about', 1000)}> About </li>
+          <li onClick={() => this.smoothScrollTo('skills', 1000)}> Skills </li>
+          <li onClick={() => this.smoothScrollTo('projects', 1000)}> Projects </li>
+          <li onClick={() => this.smoothScrollTo('contact', 1000)}> Contact </li>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
