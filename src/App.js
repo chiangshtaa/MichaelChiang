f710b5d387be13705c3eb945d2256d4b1d835d9f
Michael <chiangshtaa@gmail.com>
2018-11-07 14:11:02 -0800

    add about me information

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
-import './App2.css';
+import './App.css';
 
 
 class App extends Component {
