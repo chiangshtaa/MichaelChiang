1f66a3340f8161a870945f66d8fbbfb95c824dcf
Michael <chiangshtaa@gmail.com>
2018-11-10 15:04:52 -0800

    update contact form with transition of placeholder on focus and if valid

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
-import './css/Contact.css';
+// import './css/Contact.css';
+
+import './css/Contact2.css';
 
 class Contact extends Component {
   render() {
@@ -11,19 +13,32 @@ class Contact extends Component {
           <div className="contact-container">
 
             <div className="contact-info">
-              <input className="input name" type="text" placeholder="Your Name"/>
-              <input className="input email" type="text" placeholder="Email Address"/>
+              <form>
+                <div className="contacter-info">
+                  <div className="name">
+                    <input type="text" name="" required/>
+                    <label>Name</label>
+                  </div>
+
+                  <div className="email">
+                    <input type="text" name="" required/>
+                    <label>Email</label>
+                  </div>
+                </div>
+
+                <div>
+                  <input type="text" name="" required/>
+                  <label>Subject</label>
+                </div>
+                <div>
+                  <textarea required></textarea>
+                  <label>Message</label>
+                </div>
+
+                <input type="submit" name="" value="Send"/>
+              </form>
             </div>
 
-            <div className="subject">
-              <input className="input subject" type="text" placeholder="Subject"/>
-            </div>
-
-            <div className="message">
-              <textarea className="text-area input" placeholder="Message"></textarea>
-            </div>
-
-            <button className="button" type="button">Send</button>
           </div>
         </div>
       </div>
@@ -32,3 +47,32 @@ class Contact extends Component {
 }
 
 export default Contact;
+
+// class Contact extends Component {
+//   render() {
+//     return (
+//       <div className="Contact scroll-container" id="contact">
+//         <div className="ContactMe">
+//           <div className="header">Contact Me</div>
+//           <div className="contact-container">
+
+//             <div className="contact-info">
+//               <input className="input name" type="text" placeholder="Your Name"/>
+//               <input className="input email" type="text" placeholder="Email Address"/>
+//             </div>
+
+//             <div className="subject">
+//               <input className="input subject" type="text" placeholder="Subject"/>
+//             </div>
+
+//             <div className="message">
+//               <textarea className="text-area input" placeholder="Message"></textarea>
+//             </div>
+
+//             <button className="button" type="button">Send</button>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
