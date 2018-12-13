b0c32b4fbf87e1f147e2aa51f3a24391ddf176c6
Michael <chiangshtaa@gmail.com>
2018-12-12 17:53:09 -0800

    add margin

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -62,76 +62,77 @@ class Skills extends Component {
       <div className="Skills scroll-container" id="skills">
         <div className="SkillsMe">
           <div className="header">Skills & Experience</div>
-          <div className="skills-container">
-            <div className="info-container">
-              <div className="skills-info">
-                <div>
-                  My main area of expertise is web development.  I work mostly 
-                  with Javascript, ReactJS, HTML, and CSS to build web apps.  I 
-                  also have experience in backend technologies including NodeJS/Express 
-                  and both relational and non-relational databases.
-                </div>
-                <br/>
-                <div>
-                  I actively stay up to date on new industry technologies, which 
-                  allows me to stay relevant in the rapidly changing tech industry.  
-                  Building user-friendly web pages and applications is truly a passion 
-                  of mine and I am confident that both my technical skills and work 
-                  ethics will be of value to your company.
-                </div>
-                <br/>
-                <div>
-                  <span>Please check out my </span>
-                  <a href="https://www.linkedin.com/in/michaelchiang3/">
-                    <span className="linkedIn">
-                      LinkedIn
-                    </span>
-                  </a>
-                  <span> profile or </span>
-                  <div id="contactMenu" className="menu skills-page-contact">
-                    contact
-                  </div>
-                  <span className="end">&nbsp;me!</span>
-                </div></div>
-            </div>
-          </div>
 
           <div className="wheel-container">
-
-            <div className="controls-container">
-              <div className="controls">
-                <button className="switch" type="button" onClick={() => this.switch()}>Try Me!</button>
-                <br/>
-                <br/>
-                { !this.state.icon ? 
-                  <input 
-                    className="slider icon" 
-                    step="10" 
+            <div className="skills-container">
+              <div className="info-container">
+                <div className="skills-info">
+                  <div>
+                    My main area of expertise is web development.  I work mostly 
+                    with Javascript, ReactJS, HTML, and CSS to build web apps.  I 
+                    also have experience in backend technologies including NodeJS/Express 
+                    and both relational and non-relational databases.
+                  </div>
+                  <br/>
+                  <div>
+                    I actively stay up to date on new industry technologies, which 
+                    allows me to stay relevant in the rapidly changing tech industry.  
+                    Building user-friendly web pages and applications is truly a passion 
+                    of mine and I am confident that both my technical skills and work 
+                    ethics will be of value to your company.
+                  </div>
+                  <br/>
+                  <div>
+                    <span>Please check out my </span>
+                    <a href="https://www.linkedin.com/in/michaelchiang3/">
+                      <span className="linkedIn">
+                        LinkedIn
+                      </span>
+                    </a>
+                    <span> profile or </span>
+                    <div id="contactMenu" className="menu skills-page-contact">
+                      contact
+                    </div>
+                    <span className="end">&nbsp;me!</span>
+                  </div>
+                </div>
+              <div className="controls-container">
+                <div className="controls">
+                  <button className="switch" type="button" onClick={() => this.switch()}>Try Me!</button>
+                  <br/>
+                  <br/>
+                  { !this.state.icon ? 
+                    <input 
+                      className="slider icon" 
+                      step="10" 
+                      type="range" 
+                      min="0" 
+                      max="100" 
+                      value={this.state.iconSize} 
+                      onChange={(e) => this.changeIconSize(e)}/> :
+                    <input 
+                    className="slider font" 
+                    step="2" 
                     type="range" 
-                    min="0" 
-                    max="100" 
-                    value={this.state.iconSize} 
-                    onChange={(e) => this.changeIconSize(e)}/> :
-                  <input 
-                  className="slider font" 
-                  step="2" 
-                  type="range" 
-                  min="12" 
-                  max="36" 
-                  value={this.state.fontSize} 
-                  onChange={(e) => this.changeFontSize(e)}/>
-                }
-                <div className="slider-description">{this.state.title}</div>
+                    min="12" 
+                    max="36" 
+                    value={this.state.fontSize} 
+                    onChange={(e) => this.changeFontSize(e)}/>
+                  }
+                  <div className="slider-description">{this.state.title}</div>
 
-                <input className="slider" step="5" type="range" min="20" max="75" value={this.state.radius} onChange={(e) => this.changeRadius(e)}/>
-                <div className="slider-description radius">Radius</div>
+                  <input className="slider" step="5" type="range" min="20" max="75" value={this.state.radius} onChange={(e) => this.changeRadius(e)}/>
+                  <div className="slider-description radius">Radius</div>
+                </div>
               </div>
-            </div>
+              </div>
 
+          </div>
+          <div className="sphere-container">
             <div className="sphere">
               <div id="tag-cloud"></div>
             </div>
-
+          </div>
           </div>
 
         </div>
