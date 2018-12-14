3b901b35da747ba8c2ebf8a7700eccd9abf22e2b
Michael <chiangshtaa@gmail.com>
2018-12-13 17:58:00 -0800

    styled new projects page

--- client/src/components/Projects2.js
+++ client/src/components/Projects2.js
@@ -19,14 +19,28 @@ class Projects extends Component {
           stack: ['Javascript', 'React', 'React-Router', 'Redux', 'HTML5', 'CSS3', 'Semantic-UI-React', 'Node', 'Express', 'PostgreSQL', 'Amazon S3', 'Heroku']
         },
         {
+          name: 'Trello Replica',
+          image: Trello,
+          description: 'This project is my attempt at a replica of Trello.  I built this app because I felt like it’s an excellent tool for project management in regards to organization and efficiency.  It helped me organize what tasks needed to be done and who was working on them at the time.',
+          stack: ['Javascript', 'React', 'HTML5', 'CSS3']
+
+        },
+        {
           name: 'Paired',
           image: Paired,
-          description: 'Pitch Perfect is a tonal analysis learning app that allows users to progress through decks of flashcards where they can compare their pronunciation of a Mandarin word to a native speaker\'s pronunciation of that word.',
-          stack: ['Javascript', 'React', 'React-Router', 'Redux', 'HTML5', 'CSS3', 'Semantic-UI-React', 'Node', 'Express', 'PostgreSQL', 'Amazon S3', 'Heroku']
+          description: 'Paired is a food app that generates recipes based on food ingredient. Users can search using the search bar or by taking a photo of an ingredient and Paired will recommend recipes that include that food item. Additionally, it also provides wine and beer pairings for those recipes.',
+          stack: ['Javascript', 'React', 'HTML5', 'CSS3', 'Node', 'Express', 'MongoDB', 'Heroku']
+        },
+        {
+          name: 'Crypto',
+          image: CryptoApp,
+          description: 'Crypto is a simple app that generates basic information about the current top 25 coins.  I utilized coinmarketcap\'s API to obtain the information.',
+          stack: ['Javascript', 'React', 'HTML5', 'CSS3', 'Node', 'Express', 'RESTfulAPI']
         },
       ]      
     }
   }
+
   render() {
     return (
       <div className="Projects scroll-container" id="projects">
@@ -35,75 +49,48 @@ class Projects extends Component {
           <div className="container-fluid">
             <div className="row blog-list">
               <div className="col-md-6">
-                <div className="single-box">
-                  <figure><img src={PitchPerfect} alt=""/></figure>
-                  <h3>Pitch Perfect</h3>
-                  <p>
-                    Pitch Perfect is a tonal analysis learning 
-                    app that allows users to progress through decks of flashcards 
-                    where they can compare their pronunciation of a Mandarin word 
-                    to a native speaker's pronunciation of that word.
-                  </p>
-                  <ul>
-                    <li>Javascript</li>
-                    <li>React</li>
-                    <li>React-Router</li>
-                    <li>Redux</li>
-                    <li>HTML5</li>
-                    <li>CSS3</li>
-                    <li>Semantic-UI React</li>
-                    <li>Node</li>
-                    <li>Express</li>
-                    <li>PostgreSQL</li>
-                    <li>Amazon S3</li>
-                    <li>Heroku</li>
-                  </ul>
-                </div>
+                {
+                  this.state.projects.slice(0, 2).map((project) => {
+                    return (
+                      <div className="single-box">
+                        <figure><img src={project.image} alt=""/></figure>
+                        <h3>{project.name}</h3>
+                        <p>{project.description}</p>
+                        <ul>
+                          {
+                            project.stack.map((tech) => {
+                              return (
+                                <li>{tech}</li>
+                              )
+                            })
+                          }
+                        </ul>
+                      </div>
+                    )
+                  })
+                }
               </div>
               <div className="col-md-6">
-                <div className="single-box">
-                  <figure><img src={Paired} alt=""/></figure>
-                  <ul>
-                    <li><i className="fa fa-calendar-o" aria-hidden="true"></i> Dec 17, 2016</li>
-                    <li><i className="fa fa-user-o" aria-hidden="true"></i> Admin</li>
-                    <li><i className="fa fa-comments-o" aria-hidden="true"></i> Featured</li>
-                  </ul>
-                  <h3>It was popularised in the 1960s</h3>
-                  <p>
-                      t's time to play the music. It's time to light the lights. It's time to meet the Muppets on the Muppet Show tonight! Boy the way Glen Miller played Songs the hit parade. 
-                  </p>
-                  <span className="more">Read More <i className="fa fa-angle-double-right" aria-hidden="true"></i></span>
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div className="single-box">
-                  <figure><img src={Trello} alt=""/></figure>
-                  <ul>
-                    <li><i className="fa fa-calendar-o" aria-hidden="true"></i> Dec 17, 2016</li>
-                    <li><i className="fa fa-user-o" aria-hidden="true"></i> Admin</li>
-                    <li><i className="fa fa-comments-o" aria-hidden="true"></i> Featured</li>
-                  </ul>
-                  <h3>It was popularised in the 1960s</h3>
-                  <p>
-                      t's time to play the music. It's time to light the lights. It's time to meet the Muppets on the Muppet Show tonight! Boy the way Glen Miller played Songs the hit parade. 
-                  </p>
-                  <span className="more">Read More <i className="fa fa-angle-double-right" aria-hidden="true"></i></span>
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div className="single-box">
-                  <figure><img src={CryptoApp} alt=""/></figure>
-                  <ul>
-                    <li><i className="fa fa-calendar-o" aria-hidden="true"></i> Dec 17, 2016</li>
-                    <li><i className="fa fa-user-o" aria-hidden="true"></i> Admin</li>
-                    <li><i className="fa fa-comments-o" aria-hidden="true"></i> Featured</li>
-                  </ul>
-                  <h3>It was popularised in the 1960s</h3>
-                  <p>
-                      t's time to play the music. It's time to light the lights. It's time to meet the Muppets on the Muppet Show tonight! Boy the way Glen Miller played Songs the hit parade. 
-                  </p>
-                  <span className="more">Read More <i className="fa fa-angle-double-right" aria-hidden="true"></i></span>
-                </div>
+                {
+                  this.state.projects.slice(2, 4).map((project) => {
+                    return (
+                      <div className="single-box">
+                        <figure><img src={project.image} alt=""/></figure>
+                        <h3>{project.name}</h3>
+                        <p>{project.description}</p>
+                        <ul>
+                          {
+                            project.stack.map((tech) => {
+                              return (
+                                <li>{tech}</li>
+                              )
+                            })
+                          }
+                        </ul>
+                      </div>
+                    )
+                  })
+                }
               </div>
             </div>
           </div>
