f4e72d23ff2c330b8fff50de389dc14cdefb681c
Michael <chiangshtaa@gmail.com>
2018-12-13 18:10:11 -0800

    add unique key for map in react

--- client/src/components/Projects2.js
+++ client/src/components/Projects2.js
@@ -16,26 +16,29 @@ class Projects extends Component {
           name: 'Pitch Perfect',
           image: PitchPerfect,
           description: 'Pitch Perfect is a tonal analysis learning app that allows users to progress through decks of flashcards where they can compare their pronunciation of a Mandarin word to a native speaker\'s pronunciation of that word.',
-          stack: ['Javascript', 'React', 'React-Router', 'Redux', 'HTML5', 'CSS3', 'Semantic-UI-React', 'Node', 'Express', 'PostgreSQL', 'Amazon S3', 'Heroku']
+          stack: ['Javascript', 'React', 'React-Router', 'Redux', 'HTML5', 'CSS3', 'Semantic-UI-React', 'Node', 'Express', 'PostgreSQL', 'Amazon S3', 'Heroku'],
+          id: 0
         },
         {
           name: 'Trello Replica',
           image: Trello,
           description: 'This project is my attempt at a replica of Trello.  I built this app because I felt like it’s an excellent tool for project management in regards to organization and efficiency.  It helped me organize what tasks needed to be done and who was working on them at the time.',
-          stack: ['Javascript', 'React', 'HTML5', 'CSS3']
-
+          stack: ['Javascript', 'React', 'HTML5', 'CSS3'],
+          id: 1
         },
         {
           name: 'Paired',
           image: Paired,
           description: 'Paired is a food app that generates recipes based on food ingredient. Users can search using the search bar or by taking a photo of an ingredient and Paired will recommend recipes that include that food item. Additionally, it also provides wine and beer pairings for those recipes.',
-          stack: ['Javascript', 'React', 'HTML5', 'CSS3', 'Node', 'Express', 'MongoDB', 'Heroku']
+          stack: ['Javascript', 'React', 'HTML5', 'CSS3', 'Node', 'Express', 'MongoDB', 'Heroku'],
+          id: 2
         },
         {
           name: 'Crypto',
           image: CryptoApp,
           description: 'Crypto is a simple app that generates basic information about the current top 25 coins.  I utilized coinmarketcap\'s API to obtain the information.',
-          stack: ['Javascript', 'React', 'HTML5', 'CSS3', 'Node', 'Express', 'RESTfulAPI']
+          stack: ['Javascript', 'React', 'HTML5', 'CSS3', 'Node', 'Express', 'RESTfulAPI'],
+          id: 3
         },
       ]      
     }
@@ -43,15 +46,15 @@ class Projects extends Component {
 
   displayProjects(project) {
     return (
-      <div className="single-box">
+      <div className="single-box" key={project.id}>
         <figure><img src={project.image} alt=""/></figure>
         <h3>{project.name}</h3>
         <p>{project.description}</p>
         <ul>
           {
-            project.stack.map((tech) => {
+            project.stack.map((tech, index) => {
               return (
-                <li>{tech}</li>
+                <li key={index}>{tech}</li>
               )
             })
           }
