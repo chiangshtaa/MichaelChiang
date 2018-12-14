376f3022a3a2853885f0a2ebd51fcec4b7a369e0
Michael <chiangshtaa@gmail.com>
2018-12-13 20:07:19 -0800

    social media links open in new tab

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -54,8 +54,8 @@ class Navbar extends Component {
           <div id="skillsMenu" className="menu"> Skills </div>
           <div id="contactMenu" className="menu"> Contact </div>
           <div className="social-media">
-            <a href="https://www.linkedin.com/in/michaelchiang3/"><i class="fab fa-linkedin-in"></i></a>
-            <a href="https://github.com/chiangshtaa"><i class="fab fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/michaelchiang3/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://github.com/chiangshtaa" target="_blank"><i class="fab fa-github"></i></a>
           </div>
         </div>
       </div>
